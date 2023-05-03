b6a83f5676a41250ab5cb53b5575b08ed87b70a4
Ahmad Mohammed Abdelhameed <abdelhameed9@yahoo.com>
2023-05-03 20:39:27 +0300

    add admin reset password notification

---
+++ app/Notifications/AdminPasswordNotification.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class AdminPasswordNotification extends Notification
+{
+    public $token;
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->line('This link for admin to reset his/her password')
+                    ->action('Admin Reset Password', url('/back/reset-password',$this->token))
+                    ->line('This password reset link will expire in 60 minutes.')
+                    ->line('If you did not request a password reset, no further action is required.');
+
+
+
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
