8e90c1a0b9430d5e2cf0ee2c92a63df4cccb2f79
Ahmad Mohammed Abdelhameed <abdelhameed9@yahoo.com>
2023-05-03 18:25:19 +0300

    login and logout functions finished

--- app/Http/Controllers/AdminAuth/AuthenticatedSessionController.php
+++ app/Http/Controllers/AdminAuth/AuthenticatedSessionController.php
@@ -26,7 +26,7 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request): RedirectResponse
     {
-        $request->authenticate();
+        $request->authenticate('admin');
 
         $request->session()->regenerate();
 
@@ -40,12 +40,12 @@ class AuthenticatedSessionController extends Controller
      */
     public function destroy(Request $request): RedirectResponse
     {
-        Auth::guard('web')->logout();
+        Auth::guard('admin')->logout();
 
         $request->session()->invalidate();
 
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        return redirect('/back/login');
     }
 }
